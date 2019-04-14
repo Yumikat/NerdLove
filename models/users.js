f708f30e3e96a44b175bb894bdc7f3cb0bca7840
Pidchaya <yueyblah@hotmail.com>
2019-04-14 11:12:49 -0600

    api and html routes

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
+        name: DataTypes.STRING,
         userName: DataTypes.STRING,
         picture: DataTypes.STRING,
         bio: DataTypes.TEXT,
