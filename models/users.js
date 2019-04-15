77ec953cbd8d75470c9d0ebc9e81c7c657632475
yuliyazabarsky <yuliya1975@msn.com>
2019-04-15 09:14:18 -0600

    adding seeds

--- models/users.js
+++ models/users.js
@@ -1,9 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
+        name:DataTypes.STRING,
         userName: DataTypes.STRING,
-        picture: DataTypes.STRING,
+        picture: DataTypes.TEXT,
         bio: DataTypes.TEXT,
+        gender: DataTypes.STRING,
+        age: DataTypes.INTEGER,
         answers: DataTypes.STRING
     });
     return User;
 }
+
