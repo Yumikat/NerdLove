5edb3145e06f4a3e7f288698f3a1ed3d3e1a633f
Pidchaya <yueyblah@hotmail.com>
2019-04-16 10:42:52 -0600

    changed seeds

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 var passport = require('passport');
-var GitHubStrategy = require('passport-github2').Strategy;
+// var GitHubStrategy = require('passport-github2').Strategy;
 var LocalStrategy = require('passport-local').Strategy;
 
 var db = require("../models");
@@ -29,18 +29,18 @@ passport.use(new LocalStrategy(
 ));
 
 
-passport.use(new GitHubStrategy({
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/github/matches"
-    },
-    function(accessToken, refreshToken, profile, done) {
-        console.log(profile);
-        db.User.findOrCreate({ githubId: profile.id }, function(err, user) {
-            return done(err, user);
-        });
-    }
-));
+// passport.use(new GitHubStrategy({
+//         clientID: process.env.GITHUB_CLIENT_ID,
+//         clientSecret: process.env.GITHUB_CLIENT_SECRET,
+//         callbackURL: "http://localhost:3000/auth/github/matches"
+//     },
+//     function(accessToken, refreshToken, profile, done) {
+//         console.log(profile);
+//         db.User.findOrCreate({ githubId: profile.id }, function(err, user) {
+//             return done(err, user);
+//         });
+//     }
+// ));
 
 passport.serializeUser(function(user, cb) {
     cb(null, user);
